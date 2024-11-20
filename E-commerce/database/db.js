a2e32d124b40e8db6c670bc7eb60703018770b40
Luis Alberto Ibarra Calderon <ibarra.luisalberto23@gmail.com>
2024-11-19 21:18:56 -0400

    ..

--- E-commerce/database/db.js
+++ E-commerce/database/db.js
@@ -2,15 +2,17 @@ const { Sequelize } = require('sequelize');
 
 // Obtener las credenciales de las variables de entorno
 const DB_NAME = process.env.DATABASE_NAME || 'ecommerce';
-const DB_USER = process.env.DATABASE_USER || 'root';
-const DB_PASSWORD = process.env.DATABASE_PASSWORD || '23luis99';
-const DB_HOST = process.env.DATABASE_HOST || 'localhost';
+const DB_USER = process.env.DATABASE_USER || 'username';  // Nombre de usuario de la base de datos en Render
+const DB_PASSWORD = process.env.DATABASE_PASSWORD || 'password';  // Contraseña de la base de datos en Render
+const DB_HOST = process.env.DATABASE_HOST || 'host_de_render';  // URL del host de la base de datos de Render
+const DB_PORT = process.env.DATABASE_PORT || 3306;  // Puerto de la base de datos
 
 // Configuración de Sequelize para MySQL
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
+  port: DB_PORT,  // Usar el puerto correcto
   dialect: 'mysql',
-  logging: false, // Opcional: Deshabilitar logs SQL
+  logging: false,  // Opcional: Deshabilitar logs SQL
 });
 
 sequelize.authenticate()
