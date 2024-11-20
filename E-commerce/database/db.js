ccbf9f627861862fa79986a3c9c3dc9b236b13f7
Luis Alberto Ibarra Calderon <ibarra.luisalberto23@gmail.com>
2024-11-19 21:55:18 -0400

    ,,,

--- E-commerce/database/db.js
+++ E-commerce/database/db.js
@@ -1,18 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-// Obtener las credenciales de las variables de entorno
-const DB_NAME = process.env.DATABASE_NAME || 'ecommerce';
-const DB_USER = process.env.DATABASE_USER || 'username';  // Nombre de usuario de la base de datos en Render
-const DB_PASSWORD = process.env.DATABASE_PASSWORD || 'password';  // Contraseña de la base de datos en Render
-const DB_HOST = process.env.DATABASE_HOST || 'host_de_render';  // URL del host de la base de datos de Render
-const DB_PORT = process.env.DATABASE_PORT || 3306;  // Puerto de la base de datos
+// Configuración de conexión usando tus credenciales locales
+const DB_NAME = 'ecommerce';
+const DB_USER = 'root';
+const DB_PASSWORD = '23luis99';
+const DB_HOST = '127.0.0.1';  // Asegúrate de usar la IP local de tu máquina
 
-// Configuración de Sequelize para MySQL
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  port: DB_PORT,  // Usar el puerto correcto
   dialect: 'mysql',
-  logging: false,  // Opcional: Deshabilitar logs SQL
+  port: 3306, // Puerto MySQL
+  logging: false, // Opcional: Deshabilitar logs SQL
 });
 
 sequelize.authenticate()
@@ -20,7 +18,8 @@ sequelize.authenticate()
     console.log('Conexión exitosa a la base de datos MySQL');
   })
   .catch(err => {
-    console.error('No se pudo conectar a la base de datos:', err);
+    console.error('Error al conectar a la base de datos:', err);
   });
 
 module.exports = sequelize;
+
